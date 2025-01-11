c3abb3ca36653cffad60b5912c5eba6796a156bf
dev-world-rohit <rohitdewaliya02@gmail.com>
2025-01-11 08:10:59 +0530

    add pause functionality

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ pygame.display.set_icon(icon)
 
 # Images-----------------------------#
 white_overlay = scale_image_size(load_image('white_background_overlay.png', 30), SCREEN_WIDTH, SCREEN_HEIGHT)
+black_overlay = scale_image_size(load_image('black_background_overlay.png', 30), SCREEN_WIDTH, SCREEN_HEIGHT)
 
 road_background = load_image('road_background.png')
 
@@ -28,15 +29,19 @@ heart = load_image('heart.png')
 heart = scale_image_ratio(heart, 2)
 
 button_scale = 0.8
-yellow_music_on_button = scale_image_ratio(load_image('buttons/black_buttons/yellow_music_on.png'), button_scale)
-yellow_music_off_button = scale_image_ratio(load_image('buttons/black_buttons/yellow_music_off.png'), button_scale)
-yellow_play_button = scale_image_ratio(load_image('buttons/black_buttons/yellow_play.png'), button_scale)
-yellow_thumbs_up_button = scale_image_ratio(load_image('buttons/black_buttons/yellow_thumbs_up.png'), button_scale)
+yellow_music_on_button = scale_image_ratio(load_image('buttons/yellow_buttons/yellow_music_on.png'), button_scale)
+yellow_music_off_button = scale_image_ratio(load_image('buttons/yellow_buttons/yellow_music_off.png'), button_scale)
+yellow_play_button = scale_image_ratio(load_image('buttons/yellow_buttons/yellow_play.png'), button_scale)
+yellow_thumbs_up_button = scale_image_ratio(load_image('buttons/yellow_buttons/yellow_thumbs_up.png'), button_scale)
+yellow_pause_button = scale_image_ratio(load_image('buttons/yellow_buttons/yellow_pause.png'), button_scale - 0.2)
+yellow_replay_button = load_image('buttons/yellow_buttons/yellow_replay.png')
 
 white_music_on_button = scale_image_ratio(load_image('buttons/white_buttons/white_music_on.png'), button_scale)
 white_music_off_button = scale_image_ratio(load_image('buttons/white_buttons/white_music_off.png'), button_scale)
 white_play_button = scale_image_ratio(load_image('buttons/white_buttons/white_play.png'), button_scale)
 white_thumbs_up_button = scale_image_ratio(load_image('buttons/white_buttons/white_thumbs_up.png'), button_scale)
+white_pause_button = scale_image_ratio(load_image('buttons/white_buttons/white_pause.png'), button_scale - 0.2)
+white_replay_button = load_image('buttons/white_buttons/white_replay.png')
 
 buttons = {
     'music_on': [yellow_music_on_button, white_music_on_button, [SCREEN_WIDTH - 50, 10]],
@@ -47,6 +52,12 @@ buttons = {
     'thumbs_up': [yellow_thumbs_up_button, white_thumbs_up_button, [(SCREEN_WIDTH - yellow_play_button.get_width())
                                                                       // 2 + yellow_play_button.get_width(),
                                                     (SCREEN_HEIGHT - yellow_play_button.get_height()) // 2]]
+}
+
+additional_buttons = {
+    'pause': [yellow_pause_button, white_pause_button, [SCREEN_WIDTH - 50, 10]],
+    'replay': [yellow_replay_button, white_replay_button, [(SCREEN_WIDTH - yellow_replay_button.get_width()) // 2,
+                                                           (SCREEN_HEIGHT - yellow_replay_button.get_height()) // 2]]
 }
 
 # Music-----------------------------#
@@ -64,6 +75,33 @@ background_music = pygame.mixer.music.load('data/audio/background_music.mp3')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.4)
 
+def game_pause():
+    game = True
+    click = False
+    while game:
+        mouse_pos = pygame.mouse.get_pos()
+        screen.blit(black_overlay, (0, 0))
+
+        button = 'replay'
+        button_data = additional_buttons[button]
+        if (button_data[2][0] < mouse_pos[0] < button_data[2][0] + button_data[0].get_width() and button_data[2][1] <
+                mouse_pos[1]
+                < button_data[2][1] + button_data[0].get_height()):
+            screen.blit(button_data[1], button_data[2])
+            if click:
+                game = False
+                click = False
+        else:
+            screen.blit(button_data[0], button_data[2])
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                game = False
+            if event.type == MOUSEBUTTONDOWN:
+                click = True
+
+        pygame.display.update()
+        clock.tick(FPS)
 
 def apply_shake(shake_duration):
     if shake_duration > 0:
@@ -79,7 +117,6 @@ main_menu_text = Font('large_font.png', (10, 0, 0), 1.5)
 # Constants-----------------------------#
 clock = pygame.time.Clock()
 FPS = 60
-
 
 player_size = [200, 50]
 player = Player([(SCREEN_WIDTH - player_size[0]) // 2, SCREEN_HEIGHT - player_size[1] * 5], player_size)
@@ -173,6 +210,7 @@ while running:
                     play_sound(failed)
                     shake_duration = 20
                     game_start = True
+                    # pygame.time.wait(100)
 
     player.display_player(screen, offset_x, offset_y)
 
@@ -182,12 +220,26 @@ while running:
         if event.type == MOUSEBUTTONDOWN:
             if not game_start:
                 player.change_color()
+                button_click = True
             else:
                 button_click = True
                 #game_start = False
 
     if shake_duration > 0:
         shake_duration -= 1
+
+    if not game_start:
+        button = 'pause'
+        button_data = additional_buttons[button]
+        if (button_data[2][0] < mouse_pos[0] < button_data[2][0] + button_data[0].get_width() and button_data[2][1] < mouse_pos[1]
+                < button_data[2][1] + button_data[0].get_height()):
+            screen.blit(button_data[1], button_data[2])
+            if button_click:
+                game_pause()
+                button_click = False
+        else:
+            button_click = False
+            screen.blit(button_data[0], button_data[2])
 
     if game_start:
         screen.blit(white_overlay, (0, 0))
